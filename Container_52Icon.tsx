5806fe3396a8266bd5ff9096ba5342d3949c95ce
jakisoba <155752321+jakisoba@users.noreply.github.com>
2024-05-18 13:34:53 +0200

    Add files via upload

---
+++ Container_52Icon.tsx
@@ -0,0 +1,10 @@
+import { memo, SVGProps } from 'react';
+
+const Container_52Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 1282 641' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <circle cx={50} cy={50} r={50} fill='#D9D9D9' />
+  </svg>
+);
+
+const Memo = memo(Container_52Icon);
+export { Memo as Container_52Icon };
---
+++ Thegoonies.tsx
@@ -0,0 +1,341 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import { _5d25c68703e41086eb3b2da1_keyM } from './_5d25c68703e41086eb3b2da1_keyM/_5d25c68703e41086eb3b2da1_keyM.js';
+import { _5d25cdda7dcddd7271ddc7da_coin } from './_5d25cdda7dcddd7271ddc7da_coin/_5d25cdda7dcddd7271ddc7da_coin.js';
+import { _5d25ce8103e4105bd13b4be8_coin } from './_5d25ce8103e4105bd13b4be8_coin/_5d25ce8103e4105bd13b4be8_coin.js';
+import { _5d25ce8103e41038cf3b4be7_coin } from './_5d25ce8103e41038cf3b4be7_coin/_5d25ce8103e41038cf3b4be7_coin.js';
+import { _GooniesNeverSayDie_typeStep0 } from './_GooniesNeverSayDie_typeStep0/_GooniesNeverSayDie_typeStep0.js';
+import { Container_52Icon } from './Container_52Icon.js';
+import { Item_2 } from './Item_2/Item_2.js';
+import { LinksvgIcon } from './LinksvgIcon.js';
+import classes from './Thegoonies.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const Thegoonies: FC<Props> = memo(function Thegoonies(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.body}>
+        <div className={classes.container_2}>
+          <div className={classes.container_3}>
+            <div className={classes.container_4}>
+              <div className={classes.container_5}>
+                <div className={classes._5d02af2ae618811d9e5cac91_The20}></div>
+              </div>
+              <div className={classes.imageMike}></div>
+              <div className={classes.overlay}></div>
+            </div>
+            <div className={classes.container_6}>
+              <div className={classes.container_7}>
+                <div className={classes._5d05918fe1a86872f1a03911_4tran}></div>
+              </div>
+              <div className={classes.imageChunk}></div>
+              <div className={classes.overlay_2}></div>
+            </div>
+            <div className={classes.container_8}>
+              <div className={classes.container_9}>
+                <div className={classes._5d058dcfe1a868bd88a033ed_3tran}></div>
+              </div>
+              <div className={classes.imageDada}></div>
+              <div className={classes.overlay_3}></div>
+            </div>
+            <div className={classes.container_10}>
+              <div className={classes.container_11}>
+                <div className={classes._5d24635d2758ae4bbc2fe2ae_y2mat}></div>
+              </div>
+              <div className={classes.imageClarque}></div>
+              <div className={classes.overlay_4}></div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.container_12}>
+          <div className={classes.coin}>
+            <div className={classes.frame}>
+              <div className={classes._5d25ce8103e41038cf3b4be7_coin1}></div>
+            </div>
+          </div>
+          <div className={classes.container_13}>
+            <div className={classes.heading1}></div>
+          </div>
+          <_5d25ce8103e41038cf3b4be7_coin classes={{ _5d25ce8103e41038cf3b4be7_coin1: classes.coinn1 }} />
+          <_5d25cdda7dcddd7271ddc7da_coin
+            className={classes.coin2}
+            classes={{ _5d25cdda7dcddd7271ddc7da_coinP: classes.coinn2 }}
+          />
+          <_5d25ce8103e4105bd13b4be8_coin
+            className={classes.coin3}
+            classes={{ _5d25ce8103e4105bd13b4be8_coin3: classes.coinn3 }}
+          />
+          <_5d25c68703e41086eb3b2da1_keyM
+            className={classes.coin4}
+            classes={{ _5d25c68703e41086eb3b2da1_keyMi: classes.coinn4 }}
+          />
+          <_GooniesNeverSayDie_typeStep0 className={classes._GooniesNeverSayDie} />
+        </div>
+        <div className={classes.container_14}>
+          <div className={classes.border}>
+            <div className={classes.container_15}>
+              <div className={classes.border_2}>
+                <div className={classes.container_16}>
+                  <div className={classes.container_17}>
+                    <div className={classes.heading2}>
+                      <div className={classes._1985}>1985</div>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.heading3margin}>
+                  <div className={classes.heading3}>
+                    <div className={classes.releasedate}>Release date</div>
+                  </div>
+                </div>
+                <div className={classes.container_18}>
+                  <div className={classes.june71985}>June 7, 1985</div>
+                </div>
+              </div>
+              <div className={classes.border_3}>
+                <div className={classes.steviespielbie}></div>
+                <div className={classes.heading3margin_2}>
+                  <div className={classes.heading3_2}>
+                    <div className={classes.storyby}>Story by</div>
+                  </div>
+                </div>
+                <div className={classes.container_19}>
+                  <div className={classes.stevenSpielberg}>Steven Spielberg</div>
+                </div>
+              </div>
+              <div className={classes.horizontalBorder}>
+                <div className={classes.container_20}>
+                  <div className={classes.andriy}></div>
+                </div>
+                <div className={classes.heading3margin_3}>
+                  <div className={classes.heading3_3}>
+                    <div className={classes.production}>Production</div>
+                  </div>
+                </div>
+                <div className={classes.container_21}>
+                  <div className={classes.amblinEntertainment}>Amblin Entertainment</div>
+                </div>
+              </div>
+              <div className={classes.verticalBorder}>
+                <div className={classes.container_22}>
+                  <div className={classes._5d0803429c4398257ab505c0_logop}></div>
+                </div>
+                <div className={classes.heading3margin_4}>
+                  <div className={classes.heading3_4}>
+                    <div className={classes.distributedby}>Distributed by</div>
+                  </div>
+                </div>
+                <div className={classes.container_23}>
+                  <div className={classes.warnerBros}>Warner Bros</div>
+                </div>
+              </div>
+              <div className={classes.verticalBorder_2}>
+                <div className={classes.container_24}>
+                  <div className={classes.container_25}>
+                    <div className={classes.heading2_2}>
+                      <div className={classes._19M}>$19M</div>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.heading3margin_5}>
+                  <div className={classes.heading3_5}>
+                    <div className={classes.budget}>Budget</div>
+                  </div>
+                </div>
+                <div className={classes.container_26}>
+                  <div className={classes._19Million}>$19 Million</div>
+                </div>
+              </div>
+              <div className={classes.verticalBorder_3}>
+                <div className={classes.container_27}>
+                  <div className={classes.dave}></div>
+                </div>
+                <div className={classes.heading3margin_6}>
+                  <div className={classes.heading3_6}>
+                    <div className={classes.musicby}>Music by</div>
+                  </div>
+                </div>
+                <div className={classes.container_28}>
+                  <div className={classes.daveGrusin}>Dave Grusin</div>
+                </div>
+              </div>
+              <div className={classes.container_29}>
+                <div className={classes.container_30}>
+                  <div className={classes.container_31}>
+                    <div className={classes.heading2_3}>
+                      <div className={classes._61M}>$61M</div>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.heading3margin_7}>
+                  <div className={classes.heading3_7}>
+                    <div className={classes.boxOffice}>Box Office</div>
+                  </div>
+                </div>
+                <div className={classes.container_32}>
+                  <div className={classes._61Million}>$61 Million</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.container_33}>
+          <div className={classes.bonehead}></div>
+          <div className={classes.people}></div>
+        </div>
+        <div className={classes.container_34}>
+          <div className={classes.container_35}>
+            <div className={classes.border_4}>
+              <div className={classes.boy}></div>
+              <div className={classes.container_36}>
+                <div className={classes.container_37}>
+                  <div className={classes.verticalDivider}></div>
+                </div>
+                <div className={classes.container_38}>
+                  <div className={classes.verticalDivider_2}></div>
+                </div>
+                <div className={classes.container_39}>
+                  <div className={classes.verticalDivider_3}></div>
+                </div>
+                <div className={classes.rectangle}></div>
+              </div>
+              <div className={classes.container_40}>
+                <div className={classes.container_42}>
+                  <div className={classes.image_7}></div>
+                  <div className={classes.image_8}></div>
+                  <div className={classes.image_9}></div>
+                  <div className={classes.image_10}></div>
+                  <div className={classes.image_11}></div>
+                  <div className={classes.image_12}></div>
+                  <div className={classes.image_13}></div>
+                  <div className={classes.image_14}></div>
+                </div>
+                <div className={classes.container_41}>
+                  <div className={classes.heading2margin}>
+                    <div className={classes.heading2_4}>
+                      <div className={classes.gallery}>Gallery</div>
+                    </div>
+                  </div>
+                  <div className={classes.background}>
+                    <div className={classes.viewGallery}>View Gallery</div>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.link}></div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.container_43}>
+          <div className={classes.container_44}>
+            <div className={classes.createdByJosephBerryWebflow}>
+              <p className={classes.labelWrapper}>
+                <span className={classes.label}>Created By </span>
+                <a className={classes.label2} href='http://joseph-berry.webflow.io/' target='_blank' rel='noreferrer'>
+                  Joseph Berry
+                </a>
+                <span className={classes.label3}> - </span>
+                <a className={classes.label4} href='https://webflow.com/' target='_blank' rel='noreferrer'>
+                  Webflow
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className={classes.container_45}>
+            <div className={classes.container_46}>
+              <div className={classes.sharethisexperience}>Share this experience</div>
+            </div>
+            <div className={classes.container_47}>
+              <div className={classes.link_2}>
+                <div className={classes.backtotop}>
+                  <a
+                    href='https://the-goonies.webflow.io/#home'
+                    target='_blank'
+                    rel='noreferrer'
+                    className={classes.textBlock}
+                  >
+                    Back to top
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className={classes.heading1margin}>
+              <div className={classes.heading1_2}>
+                <div className={classes.thegoonieswebflowio}>the-goonies.webflow.io</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.container_48}>
+          <div className={classes.sound}></div>
+        </div>
+        <div className={classes.container_49}>
+          <div className={classes.linkSVG}>
+            <LinksvgIcon className={classes.icon} />
+          </div>
+        </div>
+        <div className={classes.container_50}>
+          <div className={classes.bones}></div>
+        </div>
+      </div>
+      <div className={classes.background2}></div>
+      <div className={classes.sUJETZtknuvshiszkonfskacusvohb}>
+        <div className={classes.textBlock2}>СЮЖЕТ</div>
+        <div className={classes.textBlock3}>
+          <p className={classes.labelWrapper2}></p>
+        </div>
+        <div className={classes.textBlock4}>
+          <p></p>
+        </div>
+        <div className={classes.textBlock5}>
+          <p className={classes.labelWrapper3}></p>
+        </div>
+        <div className={classes.textBlock6}>
+          <p className={classes.labelWrapper4}>
+            <span className={classes.label5}>
+              Зіткнувшись із конфіскацією своїх будинків у районі Гун-Докс в Асторії,{' '}
+            </span>
+          </p>
+        </div>
+        <div className={classes.textBlock7}>
+          <p className={classes.labelWrapper5}>
+            <span className={classes.label6}>штат Орегон, до заміського клубу, що розшир</span>
+            <span className={classes.label7}>юється,</span>
+          </p>
+        </div>
+        <div className={classes.textBlock8}> група дітей, які називають себе «Бовдури»,</div>
+        <div className={classes.textBlock9}>
+          <p className={classes.labelWrapper6}>
+            <span className={classes.label8}> збирається ра</span>
+            <span className={classes.label9}>зом на останні вихідні.</span>
+          </p>
+        </div>
+      </div>
+      <div className={classes.container_52}>
+        <Container_52Icon className={classes.icon2} />
+      </div>
+      <div className={classes.headerBg}></div>
+      <div className={classes.header}></div>
+      <div className={classes.foreground}></div>
+      <div className={classes.list}>
+        <Item_2 />
+        <div className={classes.item_3}>
+          <div className={classes.stochniki}>Істочники</div>
+          <div className={classes.link_5}></div>
+        </div>
+        <div className={classes.item_4}></div>
+        <div className={classes.item}>
+          <div className={classes.link_3}>
+            <div className={classes.sujet}>Сюжет</div>
+          </div>
+          <div className={classes.margin}>
+            <div className={classes.background_2}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
